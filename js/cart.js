e9d984b67b8a8356476e1d7fe0db11c65beaee78
elkhailihamza <elkhailihamza331@gmail.com>
2023-10-23 00:17:29 +0100

    added some js to code, you can now remove elements from cart

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+var firstCard = document.getElementById('item1');
+var secondCard = document.getElementById('item2');
+var thirdCard = document.getElementById('item3');
+
+var remove1 = document.getElementById('remove1').addEventListener('click', function() {
+    item1.style.display = 'none';
+})
+var remove2 = document.getElementById('remove2').addEventListener('click', function() {
+    item2.style.display = 'none';
+})
+var remove3 = document.getElementById('remove3').addEventListener('click', function() {
+    item3.style.display = 'none';
+})
